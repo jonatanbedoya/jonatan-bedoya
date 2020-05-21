cbddc93c5356f9533794cab3048be1a6f5ee0133
jonatanbedoya <55255003+jonatanbedoya@users.noreply.github.com>
2020-05-21 10:58:05 -0500

    Reparando Errores

--- AnalisisNumerico/BusquedasIncrementales.java
+++ AnalisisNumerico/BusquedasIncrementales.java
@@ -2,7 +2,7 @@ package AnalisisNumerico;
 import java.util.*;
 public class BusquedasIncrementales {
 
-	private static long delta, x0, x1, fx0, fx1;
+	private static double delta, x0, x1, fx0, fx1;
 	private static int niter;
 	public static void main(String[] args) {
 		Busquedas();
@@ -10,31 +10,31 @@ public class BusquedasIncrementales {
 	public static void Busquedas() {
 		try (Scanner a = new Scanner(System.in)) {
 			System.out.print("Ingrese el valor inicial: ");
-			x0 = a.nextLong();
+			x0 = a.nextDouble();
 			System.out.print("Ingrese el valor del paso (Delta): ");
-			delta = a.nextLong();
-			System.out.print("Ingrese el número de iteraciones: ");
+			delta = a.nextDouble();
+			System.out.print("Ingrese el nÃºmero de iteraciones: ");
 			niter = a.nextInt();
 			while(niter<1 || delta==0) {
 				if(delta==0) {
 					System.out.println("El valor del paso es incorrecto: ");
 					System.out.print("Ingrese el valor del paso (Delta): ");
-					delta = a.nextLong();
+					delta = a.nextDouble();
 				}else{
-						System.out.println("el número de iteraciones es incorrecto: ");
-						System.out.print("Ingrese el número de iteraciones: ");
+						System.out.println("el nÃºmero de iteraciones es incorrecto: ");
+						System.out.print("Ingrese el nÃºmero de iteraciones: ");
 						niter = a.nextInt();
 						}
 			}
-			//fx0 = f(x0) "¿Cómo lo podemos evaluar?"
+			//fx0 = f(x0) "Â¿CÃ³mo lo podemos evaluar?"
 			x1 = x0 + delta;
-			//fx1 = f(x1) "¿Cómo lo podemos evaluar?"
+			//fx1 = f(x1) "Â¿CÃ³mo lo podemos evaluar?"
 			int count = 1;
 			while(count<niter && fx1*fx0>0) {
 				x0 = x1;
 				/** fx0=fx1 */
 				x1 = x0 + delta;
-				//fx1 = f(x1) "¿Cómo lo podemos evaluar?"
+				//fx1 = f(x1) "Â¿CÃ³mo lo podemos evaluar?"
 				count = count +1;
 				if(fx1*fx0<0) {
 					System.out.println("Hay una raiz en intervalo: "+x0+ ","+x1);
