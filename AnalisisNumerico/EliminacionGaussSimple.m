426720c66f689ad85df74240dfac7d3ec02039d0
jonatanbedoya <55255003+jonatanbedoya@users.noreply.github.com>
2020-05-21 23:01:22 -0500

    Con Octave (.m)

---
+++ AnalisisNumerico/EliminacionGaussSimple.m
@@ -0,0 +1,34 @@
+function [Aux] = EliminacionGaussSimple(A,b,p)
+  #Funciona con cualquier matriz A nxn y b nx1
+  #Se solicita el valor p donde 0 es L
+  #y para p!=0 es U
+  c = [A, b];
+  m = 0;
+  d = length(b);
+  L = zeros(d);
+  U = zeros(d);
+  for k=1:d
+    for i=k+1:d
+      if(c(k,k) == 0)
+        disp('Hay un cero en la diagonal:')
+        disp('No se pudo completar el proceso')
+      else
+        m = c(i,k)/c(k,k);
+      endif
+      if i<=d+1
+        L(i,k)= m;
+      endif
+      for j=1:d+1
+        c(i,j) = c(i,j) - m*c(k,j);
+      endfor
+      U(k,i) = c(k,i);
+      s = c(k,i);
+    endfor
+    U(k,k) = c(k,k);
+  endfor
+  if p==0
+    Aux = L;
+  else
+    Aux = U;
+  endif
+endfunction
